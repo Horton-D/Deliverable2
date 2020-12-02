3bdc878f348e520880cef75f020a9e2431a38fe7
Darrius horton <darriushorton@Darriuss-Air.attlocal.net>
2020-12-02 12:50:39 -0500

    updated

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heads = 0;
+            int tails = 0;
+            int numOfFlips = 0;
+            string userPick;
+            Random coinflip = new Random();
+
+
+
+
+            System.Console.WriteLine("Welcome to coin flip, whats your name");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Please choose heads or tails");
+            userPick = Console.ReadLine();
+
+
+            Console.WriteLine(name + " How many times would you like to flip a coin? ");
+            numOfFlips = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+            for (int i = 0; i < numOfFlips; i++)
+            {
+
+
+                int flip = coinflip.Next(1, 3);
+                if (flip == 1)
+                {
+                    heads++;
+                    Console.WriteLine("Heads");
+                }
+                else
+                {
+                    tails++;
+                    Console.WriteLine("Tails");
+                    
+                }
+
+            }
+            Console.WriteLine("User chose " + userPick);
+
+            Console.WriteLine("You flipped a coin " + numOfFlips
+               + " times " + "and you got " + heads + " heads and " + tails + " tails.");
+
+            int percentage = (heads / numOfFlips) * 100;
+            Console.WriteLine(percentage);
+
+
+        }
+    }
+}
